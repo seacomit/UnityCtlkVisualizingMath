1c3a0437e2734b024b0c5ebf79e83ca2c757b5fa
versatus <86527888+seacomit@users.noreply.github.com>
2023-06-21 11:24:33 -0400

    Add a configurable resolution and increase the graphs resolution to 50

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -5,12 +5,21 @@ public class Graph : MonoBehaviour
     [SerializeField]
     Transform pointPrefab;
 
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+
     void Awake()
     {
-        for (int i = 0; i < 10;  i++) {
+        float step = 2f / resolution;
+        Vector3 position = Vector3.zero;
+        var scale = Vector3.one * step;
+        for (int i = 0; i < resolution;  i++) {
             Transform point = Instantiate(pointPrefab);
-            point.localPosition = Vector3.right * ((i + 0.5f) / 5f - 1f);
-            point.localScale = Vector3.one / 5f;
+            position.x = (i + 0.5f) * step - 1f;
+            position.y = position.x * position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
         }
     }
 }
